cc6a029dd5bda2c926bf8e54385ea422ab7a34bc
Kratos <desmond8500@gmail.com>
2020-09-11 09:59:30 +0000

    image update

--- resources/views/0 yonkou/index.blade.php
+++ resources/views/0 yonkou/index.blade.php
@@ -6,7 +6,11 @@
             <div class="col-md-3 mb-2">
                 <a href="{{ $site->link }}" target="_blank">
                     <div class="card text-left">
-                        <img class="card-img-top" src="holder.js/100px180/" alt="">
+                        {{-- @if ($site->logo)
+                            <img class="card-img-top" src="{{ $site->logo }}" alt="">
+                        @else
+                            <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fhamiltonrising.com%2Fartist-of-the-week-geoff-hudson-band%2Fwebsite-logo-png%2F&psig=AOvVaw3GQ27iG3XLXUdJw3xsg24M&ust=1599904711430000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDDrdfr4OsCFQAAAAAdAAAAABAD" alt="">
+                        @endif --}}
                         <div class="card-body">
                             <h4 class="card-title">{{ $site->name }}</h4>
                             <p class="card-text">{{ $site->description }}</p>
